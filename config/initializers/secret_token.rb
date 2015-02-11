e27212a3141eb07d18f4e3cbf9c3b7af082f385d
Volodymyr <Volodymyr_Antonichuk@epam.com>
2015-02-12 00:13:23 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avp::Application.config.secret_token = 'ccaea4568872f182574b62f0fd4f3ddfd7fa9729c556ec3d21b62d040712d1d239f1479bbc1e7311bb735e7a8952cc47d673d4913b7c75811d3f02f4b87f9829'
